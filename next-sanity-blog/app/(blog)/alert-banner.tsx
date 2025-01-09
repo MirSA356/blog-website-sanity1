1ce032312d0b1da07847928d56d46c898d4020cf
MirSA356 <miratifali353@gmail.com>
2025-01-09 18:32:25 +0500

    commit

--- next-sanity-blog/app/(blog)/alert-banner.tsx
+++ next-sanity-blog/app/(blog)/alert-banner.tsx
@@ -1,5 +1,7 @@
 // "use client";
 
+import Link from "next/link";
+
 // import { useRouter } from "next/navigation";
 // import { useSyncExternalStore, useTransition } from "react";
 
@@ -24,7 +26,7 @@ export default function AlertBanner() {
       className=
       // {`${
       //   pending ? "animate-pulse" : ""}
-         "fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur"
+         "fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur flex items-center justify-between px-4 h-15"
     >
       <div className="py-2 text-center text-sm">
         Mir Atif SA
@@ -49,6 +51,12 @@ export default function AlertBanner() {
           </>
         )} */}
       </div>
+         <nav className="flex space-x-6 text-sm font-medium">
+          <Link href="/" className="hover:text-blue-500">Home</Link>
+          <Link href="/about" className="hover:text-blue-500">About</Link>
+          <Link href="/contact" className="hover:text-blue-500">Contact</Link>
+         </nav>
+
     </div>
   );
 }
